787ad4560ad04f0c3ef048a8617c304aba9d6e91
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-05-01 05:30:15 +0200

    add another func

--- paper/data_generate.py
+++ paper/data_generate.py
@@ -2,8 +2,15 @@
 
 f = lambda x  : x**2 
 
+numbers = range(15)
+squared_numbers = [num * num for num in numbers]
 
 with open('data.dat','w') as out:
   for x in range(15):
     line = f'{x} {f(x)}'
     out.write(f'{line}\n')
+
+# with open('data.dat','w') as out:
+#   for num,sq in zip(numbers ,squared_numbers):
+#     line = f'{num} {sq}'
+#     out.write(f'{line}\n')
